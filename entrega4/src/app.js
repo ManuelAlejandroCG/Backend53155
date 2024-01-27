bf346904b4eb869c5a192850023cafd08d27aecc
Manuel <manuelalejandro.cg@gmail.com>
2024-01-27 13:51:30 -0300

    entrega4

---
+++ entrega4/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+app.use("/api/products", productsRouter)
+app.use("/api/carts", cartsRouter)
+
+app.get('*', (req, res) => {
+    res.json('{"error": "Page not found."}');
+});
+
+app.listen(port, () =>{
+    console.log(`Server listenin port ${port}`)
+})
